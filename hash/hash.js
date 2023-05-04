c53fede3364d207a012fa9df08881612955d76bd
Bianca Ramos <biancakaroliner04@gmail.com>
2023-05-04 19:09:57 -0300

    Node

---
+++ hash/hash.js
@@ -0,0 +1,32 @@
+import { createHash } from 'crypto';
+
+function criaHash(senha){
+    return createHash('sha256').update(senha).digest('hex');
+}
+
+console.log(criaHash("uma string qualquer"));
+
+class Usuario {
+    constructor(nome, senha){
+        this.nome = nome;
+        this.hash = criaHash(senha);
+    }
+    autentica(nome, senha){
+        if(nome === this.nome && this.hash === criaHash(senha)){
+            console.log("Usuário autenticado com sucesso!");
+            return true;
+        } else {
+            console.log("Usuário ou senha incorretos!");
+            return false;
+        }
+    }
+}
+
+const usuario = new Usuario('bianca', 'minhaSenha')
+console.log(usuario);
+
+//Caso de sucesso
+usuario.autentica('bianca', 'minhaSenha')
+
+//Caso de fracasso
+usuario.autentica('bia', 'minhasenha')
